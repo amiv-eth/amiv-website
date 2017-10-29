90073eaae6d3634f6e663900998a2a57605ded9c
Moritz Schneider <scmoritz@student.ethz.ch>
2017-10-29 15:40:48 +0100

    Pages are now in a container with a simple navbar

---
+++ src/amiv.js
@@ -0,0 +1,7 @@
+const m = require('mithril');
+
+module.exports = {
+  view() {
+    return m('div', 'example AMIV content');
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // src/index.js
 const m = require('mithril');
 const Layout = require('./layout');
+const amiv = require('./amiv');
 
 m.route(document.body, '/', {
   '/': {
     render() {
-      return m(Layout);
+      return m(Layout, m(amiv));
     },
   },
 });
