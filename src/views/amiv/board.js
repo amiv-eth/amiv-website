e3d464a9fb70e1feaf1688869163cd47ca9dbd6b
Mathis Dedial <mdedial@student.ethz.ch>
2017-10-29 18:54:19 +0100

    Create basic structure for board page

---
+++ src/views/amiv/board.js
@@ -0,0 +1,35 @@
+const m = require('mithril');
+
+// TODO: Insert images
+
+module.exports = {
+  view() {
+    return m('div', [
+      m('h3', 'Vorstand HS17'),
+      m('h4', 'Aurel Neff - Präsident'),
+      m('p', 'Aurel Text'),
+      m('h4', 'Patrick Wintermeyer - Quästor'),
+      m('p', 'Patrick Text'),
+      m('h4', 'Sandro Lutz - IT'),
+      m('p', 'Sandro Text'),
+      m('h4', 'Mathis Dedial - Information'),
+      m('p', 'Mathis Text'),
+      m('h4', 'Ruben Stadler - Kultur'),
+      m('p', 'Ruben Text'),
+      m('h4', 'Kira Erb - Kultur'),
+      m('p', 'Kira Text'),
+      m('h4', 'Sandro Lutz - IT'),
+      m('p', 'Sandro Text'),
+      m('h4', 'Tino Gfrörer - HoPo ITET'),
+      m('p', 'Tino Text'),
+      m('h4', 'Annan Zhang - HoPo MAVT'),
+      m('p', 'Annan Text'),
+      m('h4', 'Christian Miklautz - External Relations'),
+      m('p', 'Christian Text'),
+      m('h4', 'Celina Rhonheimer - External Relations'),
+      m('p', 'Celina Text'),
+      m('h4', 'Antonia Mosberger - Infrastruktur'),
+      m('p', 'Antonia Text'),
+    ]);
+  },
+};
