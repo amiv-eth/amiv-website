2f1bfdf0bc9ce60fe5a1fd9b031b26d34dba4fbe
Mathis Dedial <mdedial@student.ethz.ch>
2017-10-29 16:57:32 +0100

    Very raw version of AMIV Statuten 2017

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // src/index.js
 const m = require('mithril');
 const Layout = require('./layout');
+const statuten = require('./amiv/statuten');
 
 m.route(document.body, '/', {
   '/': {
     render() {
       return m(Layout);
+    },
+  },
+  '/amiv/statuten': {
+    render() {
+      return m(statuten);
     },
   },
 });
