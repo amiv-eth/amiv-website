cc8a57b43dfa26bb1976cbc0d6790d3920704b6b
Moritz Schneider <scmoritz@student.ethz.ch>
2017-11-04 12:24:08 +0100

    Removed hardcoded path prefix

--- src/views/layout.js
+++ src/views/layout.js
@@ -7,28 +7,29 @@ module.exports = {
     if (auth.authenticated === false) {
       return m('div', [
         m('nav', [
-          m('a', { href: '/' }, 'AMIV'),
-          m('a', { href: '/#!/events' }, 'Events'),
-          m('a', { href: '/#!/studydocuments' }, 'Studienunterlagen'),
-          m('a', { href: '/#!/jobs' }, 'Jobs'),
-          m('a', { href: '/#!/login' }, 'Login'),
+          m('a', { href: '/', oncreate: m.route.link }, 'AMIV'),
+          m('a', { href: '/events', oncreate: m.route.link }, 'Events'),
+          m('a', { href: '/studydocuments', oncreate: m.route.link }, 'Studienunterlagen'),
+          m('a', { href: '/jobs', oncreate: m.route.link }, 'Jobs'),
+          m('a', { href: '/login', oncreate: m.route.link }, 'Login'),
         ]),
         m('main', vnode.children),
       ]);
     }
     return m('div', [
       m('nav', [
-        m('a', { href: '/' }, 'AMIV'),
-        m('a', { href: '/#!/events' }, 'Events'),
-        m('a', { href: '/#!/studydocuments' }, 'Studienunterlagen'),
-        m('a', { href: '/#!/jobs' }, 'Jobs'),
-        m('a', { href: '/#!/profile' }, 'Profil'),
+        m('a', { href: '/', oncreate: m.route.link }, 'AMIV'),
+        m('a', { href: '/events', oncreate: m.route.link }, 'Events'),
+        m('a', { href: '/studydocuments', oncreate: m.route.link }, 'Studienunterlagen'),
+        m('a', { href: '/jobs', oncreate: m.route.link }, 'Jobs'),
+        m('a', { href: '/profile', oncreate: m.route.link }, 'Profil'),
         m('a', {
-          href: '#',
+          href: '/',
           onclick: () => {
             auth.logout();
             return false;
           },
+          oncreate: m.route.link,
         }, 'Logout'),
       ]),
       m('main', vnode.children),
