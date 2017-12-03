3e099f572bbb101b7f4d108edd744a2e7779101b
Sandro Lutz <code@temparus.ch>
2017-12-03 03:10:39 +0100

    Add event signup (without additional fields)

--- src/views/eventDetails.js
+++ src/views/eventDetails.js
@@ -1,6 +1,39 @@
 import * as events from '../models/event';
+import { isLoggedIn } from '../models/auth';
 
 const m = require('mithril');
+
+let signupEmail = '';
+
+class EventSignupForm {
+  static oninit() {
+    if (isLoggedIn()) {
+      events.checkCurrentSignup();
+    }
+  }
+
+  static view() {
+    if (typeof events.getCurrent() === 'undefined') {
+      return m('div');
+    }
+    if (isLoggedIn()) {
+      if (typeof events.getCurrentSignup() === 'undefined') {
+        return m('button', { onclick() { events.signupCurrent(); } }, 'signup');
+      }
+    } else if (events.getCurrent().allow_email_signup) {
+      return m('div', [
+        m('input', {
+          type: 'text',
+          placeholder: 'Email',
+          oninput: m.withAttr('value', (value) => { signupEmail = value; }),
+          value: signupEmail,
+        }),
+        m('button', { onclick() { events.signupCurrent(signupEmail); } }, 'signup'),
+      ]);
+    }
+    return m('div');
+  }
+}
 
 export default class EventDetails {
   static oninit(vnode) {
@@ -17,6 +50,7 @@ export default class EventDetails {
       m('span', events.getCurrent().signup_count),
       m('span', events.getCurrent().spots),
       m('p', events.getCurrent().description_de),
+      m(EventSignupForm),
     ]);
   }
 }
