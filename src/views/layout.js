119238d1087b2c36566260ee92a28f747e97cbb7
Moritz Schneider <scmoritz@student.ethz.ch>
2018-02-12 09:55:51 +0100

    Simplify layout.js

--- src/views/layout.js
+++ src/views/layout.js
@@ -1,13 +1,16 @@
 import m from 'mithril';
 import { checkLogin, isLoggedIn, logout } from '../models/auth';
 
+const layoutCommon = [
+  m('a', { href: '/', oncreate: m.route.link }, 'AMIV'),
+  m('a', { href: '/events', oncreate: m.route.link }, 'Events'),
+  m('a', { href: '/studydocuments', oncreate: m.route.link }, 'Studienunterlagen'),
+  m('a', { href: '/jobs', oncreate: m.route.link }, 'Jobs'),
+];
 
 const layoutLoggedOut = vnode => m('div', [
   m('nav', [
-    m('a', { href: '/', oncreate: m.route.link }, 'AMIV'),
-    m('a', { href: '/events', oncreate: m.route.link }, 'Events'),
-    m('a', { href: '/studydocuments', oncreate: m.route.link }, 'Studienunterlagen'),
-    m('a', { href: '/jobs', oncreate: m.route.link }, 'Jobs'),
+    layoutCommon,
     m('a', { href: '/login', oncreate: m.route.link }, 'Login'),
   ]),
   m('main', vnode.children),
@@ -15,10 +18,7 @@ const layoutLoggedOut = vnode => m('div', [
 
 const layoutLoggedIn = vnode => m('div', [
   m('nav', [
-    m('a', { href: '/', oncreate: m.route.link }, 'AMIV'),
-    m('a', { href: '/events', oncreate: m.route.link }, 'Events'),
-    m('a', { href: '/studydocuments', oncreate: m.route.link }, 'Studienunterlagen'),
-    m('a', { href: '/jobs', oncreate: m.route.link }, 'Jobs'),
+    layoutCommon,
     m('a', { href: '/profile', oncreate: m.route.link }, 'Profil'),
     m('a', {
       href: '/',
@@ -26,7 +26,6 @@ const layoutLoggedIn = vnode => m('div', [
         logout().then(() => { m.route.set('/'); });
         return false;
       },
-      oncreate: m.route.link,
     }, 'Logout'),
   ]),
   m('main', vnode.children),
