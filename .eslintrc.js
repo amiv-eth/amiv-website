fb221b2b418db0b843b21e5e9f96ea0eedc863d6
Cliff Li <lic@student.ethz.ch>
2018-03-10 16:48:00 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
-    'extends': 'airbnb-base',
-    'env': {
-        'browser': true,
-        'node': true
-    },
-    'rules': {
-        'no-multi-str': 0,
-        'no-underscore-dangle': 0,
-        'no-console': 0,
-    },
+  extends: [
+      'airbnb-base',
+      "plugin:prettier/recommended"
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'no-multi-str': 0,
+    'no-underscore-dangle': 0,
+    'no-console': 0,
+    'prettier/prettier': 'warn',
+  },
+  plugins: ['prettier'],
 };
