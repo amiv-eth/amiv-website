d0a18fc89f27bd527be5039ad0d4ef2bfb31d0f3
Cliff Li <lic@student.ethz.ch>
2018-03-10 15:54:15 +0100

    Add example styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import m from "mithril";
 import { Button } from "polythene-mithril";
-import "polythene-css";
+import { ButtonCSS } from "polythene-css";
+
+ButtonCSS.addStyle(".blue-button", {
+    color_light_background: "blue",
+    color_light_text:       "white"
+})
 
 export default class ButtonComponent {
     static view(vnode) {
         const defaultProps = {
+            className: "blue-button",
             element: 'button',
             disabled: vnode.attrs.active === false,
             label: 'Unnamed button',
