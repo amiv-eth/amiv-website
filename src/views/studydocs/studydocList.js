be4de6c519c95f42b482677bd208246b10b63852
Elias Asikainen <Elias@Elias-Asikainens-MacBook-Air-2.local>
2018-03-18 12:39:57 +0100

    Add file with lecture list

--- src/views/studydocs/studydocList.js
+++ src/views/studydocs/studydocList.js
@@ -2,6 +2,7 @@ import m from 'mithril';
 import { apiUrl } from 'config';
 import * as studydocs from '../../models/studydocs';
 import { Button, Checkbox, TextField, Dropdown } from '../../components';
+import { lecture } from '../studydocs/lecture';
 
 const tableHeadings = ['title', 'type'];
 const filterNames = {
@@ -12,31 +13,6 @@ const filterNames = {
     'lecture documents': 'Unterichts Unterlagen',
     exercies: 'Übungsserien',
   },
-};
-
-const subjects = {
-  itet: [
-    ['Digitaltechnik', 'Analysis 1', 'Netzwerke und Schaltungen 1', 'Informatik 1'],
-    ['Koma'],
-    ['Physics 2'],
-    [],
-    [],
-    [],
-  ],
-  mavt: [
-    [
-      'Analysis 1',
-      'Werkstoffe und Fertigung 1',
-      'Lineare Algebra 1',
-      'Chemie',
-      'Maschinenelemente',
-    ],
-    ['Innovationsprozess'],
-    ['Dynamics', 'Thermodynamik 1', 'Philosophie'],
-    ['Fluiddynamik1', 'Thermodynamik 2'],
-    [],
-    [],
-  ],
 };
 
 export default class studydocList {
@@ -67,18 +43,18 @@ export default class studydocList {
   lectureData() {
     const data = [];
     if (this.filter.department.itet || !this.filter.department.mavt) {
-      for (let i = 0; i < subjects.itet[this.semester - 1].length; i += 1) {
+      for (let i = 0; i < lecture.itet[this.semester - 1].length; i += 1) {
         data.push({
-          id: subjects.itet[this.semester - 1][i],
-          name: subjects.itet[this.semester - 1][i],
+          id: lecture.itet[this.semester - 1][i],
+          name: lecture.itet[this.semester - 1][i],
         });
       }
     }
     if (this.filter.department.mavt || !this.filter.department.itet) {
-      for (let i = 0; i < subjects.mavt[this.semester - 1].length; i += 1) {
+      for (let i = 0; i < lecture.mavt[this.semester - 1].length; i += 1) {
         data.push({
-          id: subjects.mavt[this.semester - 1][i],
-          name: subjects.mavt[this.semester - 1][i],
+          id: lecture.mavt[this.semester - 1][i],
+          name: lecture.mavt[this.semester - 1][i],
         });
       }
     }
