1c5ff3d18831985be5788d09f06ba3d22046090e
Moritz Schneider <scmoritz@student.ethz.ch>
2018-03-28 11:27:51 +0200

    Remove "static" in company views

--- src/views/companies/companyDetail.js
+++ src/views/companies/companyDetail.js
@@ -2,21 +2,21 @@ import m from 'mithril';
 import load from '../../models/companies';
 
 export default class companyDetail {
-  static oninit(vnode) {
+  oninit(vnode) {
     this.content = '';
     load(vnode.attrs.companyId).then(response => {
       this.content = response;
     });
   }
 
-  static onbeforeupdate(vnode) {
+  onbeforeupdate(vnode) {
     // load markdown whenever component is reloaded
     load(vnode.attrs.companyId).then(response => {
       this.content = response;
     });
   }
 
-  static view() {
+  view() {
     return m.trust(this.content);
   }
 }
