1c348c552b3a497e328105813239283fcdc44d44
Sandro Lutz <code@temparus.ch>
2018-03-28 22:48:27 +0200

    Add translations

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,19 +1,14 @@
 import m from 'mithril';
+import { i18n } from '../models/language';
 
 module.exports = {
   view() {
     return m('div', [
-      m('h3', 'Kontakt'),
+      m('h3', i18n('contact')),
       m('pre', 'AMIV an der ETH\nCAB E37\nUniversitätsstrasse 6\n8092 Zürich'),
       m('pre', '+41 44 632 42 45\ninfo@amiv.ethz.ch'),
-      m(
-        'pre',
-        'Die Räumlichkeiten des AMIV befinden sich im Gebäude CAB, E-Stock, im Zentrum der ETH Zürich:'
-      ),
-      m(
-        'pre',
-        'Gewisse Dienstleistungen des AMIV wurden im Laufe der Zeit in eigene Kommissionen ausgelagert. Dies umfasst unsere Fachvereins-Zeitschrift Blitz, die Bastler (mit eigenem Bauteile-Laden und Messplatz) vom Bastli, eine eigene Bier-Braukommission, ein "Local Committee" des europäischen Netzwerks EESTEC, die Amateur-Funker von der Funkbude sowie das Organisationskomitee der AMIV-Firmenmesse Kontakt. Zudem gibt es seit dem Jahr 2009 je eine Kommission, die die Studenten der spezialisierten Masterstudiengänge "Biomedical Engineering" und "Energy Science" zusammenfassen und betreuen.'
-      ),
+      m('pre', i18n('contact.location')),
+      m('pre', i18n('contact.description')),
     ]);
   },
 };
