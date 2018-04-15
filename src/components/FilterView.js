310f1c3011cdfce84887ec68330289b37edbcb2d
Elias Asikainen <aelias@student.ethz.ch>
2018-04-15 02:57:39 +0200

    comments added

--- src/components/FilterView.js
+++ src/components/FilterView.js
@@ -37,7 +37,7 @@ export default class FilterViewComponent {
         ? m(
             'form',
             {
-              // onsubmit: ,
+              // onsubmit: , add later...
             },
             [
               m(
