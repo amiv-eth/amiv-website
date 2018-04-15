1b09140432ff34ff589839bcac9d6d4b8d214095
Elias Asikainen <aelias@student.ethz.ch>
2018-04-15 03:08:16 +0200

    fixed linter

--- src/views/studydocs/studydocList.js
+++ src/views/studydocs/studydocList.js
@@ -75,7 +75,7 @@ export default class studydocList {
   }
 
   static view() {
-    //if (!isLoggedIn()) return m(Error401);
+    if (!isLoggedIn()) return m(Error401);
 
     return m('div#studydoc-list', [
       m('div.filter', [
