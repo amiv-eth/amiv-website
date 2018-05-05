2e4349d85200df24a511faa8e1feb72f91064035
Cliff Li <lic@student.ethz.ch>
2018-05-05 12:32:57 +0200

    cleanup social media cards

--- src/views/frontpage.js
+++ src/views/frontpage.js
@@ -17,44 +17,12 @@ const renderHotCards = (item, index) => {
 };
 
 // Render the frontpage cards, with href and imageurl
-const renderCards = item => {
+const renderRowCards = item => {
   const { title, href } = item;
-  const imageurl = item.img_poster ? `${apiUrl}${item.img_poster.file}` : '';
-  return m(
-    'div.frontpage-row-card',
-    { style: `background-image: url(${imageurl})` },
-    m('a', { href }, title)
-  );
-};
-
-// Render Facebook cards, with href and imageurl
-const renderFacebookCard = item => {
-  const { title, href, imageurl } = item;
-  return m(
-    'div.frontpage-row-card',
-    { style: `background-image: url(${imageurl})` },
-    m('a', { href }, title)
-  );
-};
-
-// Render Instagram cards, with href and imageurl
-const renderInstagramCard = item => {
-  const { title, href, imageurl } = item;
-  return m(
-    'div.frontpage-row-card',
-    { style: `background-image: url(${imageurl})` },
-    m('a', { href }, title)
-  );
-};
-
-// Render SocialMedia cards, with href and imageurl
-const renderTwitterCard = item => {
-  const { title, href, imageurl } = item;
-  return m(
-    'div.frontpage-row-card',
-    { style: `background-image: url(${imageurl})` },
-    m('a', { href }, title)
-  );
+  let { imageurl } = item;
+  if (item.img_poster) imageurl = `${apiUrl}${item.img_poster.file}`;
+  const style = imageurl ? `background-image: url(${imageurl})` : '';
+  return m('div.frontpage-row-card', { style }, m('a', { href }, title));
 };
 
 export default class Frontpage {
@@ -89,25 +57,26 @@ export default class Frontpage {
     ];
 
     // Social Media Attributes, with their different APIs
-    this.facebook = {
-      title: 'Facebook',
-      href: 'https://www.facebook.com/AMIV.ETHZ/',
-      imageurl:
-        'http://www.fub.se/sites/www.fub.se/files/styles/artikelbild_full/public/facebook-logotyp.jpg?itok=e244p_Sa',
-    };
+    this.socialmedia = [
+      {
+        title: 'Facebook',
+        href: 'https://www.facebook.com/AMIV.ETHZ/',
+        imageurl:
+          'http://www.fub.se/sites/www.fub.se/files/styles/artikelbild_full/public/facebook-logotyp.jpg?itok=e244p_Sa',
+      },
 
-    this.instagram = {
-      title: 'Instagram',
-      href: 'https://www.instagram.com/amiv_eth/?hl=de',
-      imageurl:
-        'https://i2.wp.com/www.newscouch.de/wp-content/uploads/2017/11/insta-logo.jpg?fit=2569%2C1761&ssl=1',
-    };
-
-    this.twitter = {
-      title: 'Twitter',
-      href: 'https://twitter.com/amiv_ethz',
-      imageurl: 'https://rngeternal.com/wp-content/uploads/2017/12/twitter-logo.png',
-    };
+      {
+        title: 'Instagram',
+        href: 'https://www.instagram.com/amiv_eth/?hl=de',
+        imageurl:
+          'https://i2.wp.com/www.newscouch.de/wp-content/uploads/2017/11/insta-logo.jpg?fit=2569%2C1761&ssl=1',
+      },
+      {
+        title: 'Twitter',
+        href: 'https://twitter.com/amiv_ethz',
+        imageurl: 'https://rngeternal.com/wp-content/uploads/2017/12/twitter-logo.png',
+      },
+    ];
   }
 
   onbeforeupdate() {
@@ -120,15 +89,11 @@ export default class Frontpage {
       m('h2', i18n('frontpage.whats_hot')),
       m('div.hot-row', this.hot.map((item, index) => renderHotCards(item, index))),
       m('h2', i18n('Events')),
-      m('div.frontpage-row', this.events.map(item => renderCards(item))),
+      m('div.frontpage-row', this.events.map(item => renderRowCards(item))),
       m('h2', 'Jobs'),
-      m('div.frontpage-row', this.jobs.map(item => renderCards(item))),
+      m('div.frontpage-row', this.jobs.map(item => renderRowCards(item))),
       m('h2', i18n('frontpage.social_media')),
-      m('div.frontpage-row', [
-        renderFacebookCard(this.facebook),
-        renderInstagramCard(this.instagram),
-        renderTwitterCard(this.twitter),
-      ]),
+      m('div.frontpage-row', this.socialmedia.map(item => renderRowCards(item))),
     ]);
   }
 }
