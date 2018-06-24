46f13d9c1e1115bd9efd6f6d1accc6a48564f067
Sandro Lutz <code@temparus.ch>
2018-06-24 18:21:45 +0200

    Make main menu responsive (Resolves #41)

--- src/views/header.js
+++ src/views/header.js
@@ -1,100 +1,38 @@
 import m from 'mithril';
 import { mainNavigation } from '../models/navigation';
 import AmivLogo from './images/logo.svg';
+import MobileMenuButton from './images/mobileMenuButton.svg';
 import { i18n, currentLanguage, switchLanguage } from '../models/language';
 import { Button } from '../components';
 import { isLoggedIn } from '../models/auth';
 
 export default class Header {
-  static onbeforeupdate() {
+  oninit() {
+    this._mobileMenuShowing = false;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onbeforeupdate() {
     mainNavigation.onupdate();
   }
 
-  static view() {
+  view() {
     return m('header', [
       m(
         'section.blue',
-        m('div', [
-          m(
-            'a',
-            { href: `/${currentLanguage()}/`, onupdate: m.route.link },
-            m('img.logo', { src: AmivLogo })
-          ),
-          m('nav', [
+        m(
+          'nav',
+          {
+            class: this._mobileMenuShowing ? 'expanded' : '',
+          },
+          [
             m(
-              'ul.mainmenu',
-              mainNavigation.map((item, index) =>
-                m(
-                  'li',
-                  {
-                    class: mainNavigation.selectedIndex === index ? 'active' : '',
-                  },
-                  [
-                    m(
-                      'a',
-                      {
-                        href: item.getLink(),
-                        onupdate: item.onupdate,
-                      },
-                      i18n(item.label)
-                    ),
-                    item.submenu
-                      ? m('ul.submenu', [
-                          item.submenu.map((subitem, subindex) =>
-                            m(
-                              'li',
-                              { class: item.submenu.selectedIndex === subindex ? 'active' : '' },
-                              m(
-                                'a',
-                                {
-                                  href: subitem.getLink(),
-                                  onupdate: subitem.onupdate,
-                                },
-                                i18n(subitem.label)
-                              )
-                            )
-                          ),
-                        ])
-                      : m(''),
-                  ]
-                )
-              )
+              'a',
+              { href: `/${currentLanguage()}/`, onupdate: m.route.link },
+              m('img.logo', { src: AmivLogo })
             ),
-            m(
-              'ul.profile',
-              isLoggedIn()
-                ? [
-                    m(
-                      'li',
-                      {
-                        class: m.route.get() === `/${currentLanguage()}/profile` ? 'active' : '',
-                      },
-                      m(
-                        'a',
-                        { href: `/${currentLanguage()}/profile`, onupdate: m.route.link },
-                        i18n('Profile')
-                      )
-                    ),
-                    m(
-                      'li',
-                      m(
-                        'a',
-                        { href: `/${currentLanguage()}/logout`, onupdate: m.route.link },
-                        i18n('Logout')
-                      )
-                    ),
-                  ]
-                : [
-                    m(
-                      'li',
-                      m(
-                        'a',
-                        { href: `/${currentLanguage()}/profile`, onupdate: m.route.link },
-                        i18n('Login')
-                      )
-                    ),
-                  ]
-            ),
+            this.constructor._mainMenu,
+            this.constructor._profileMenu,
             m(
               'div.language-switcher',
               m(Button, {
@@ -102,10 +40,99 @@ export default class Header {
                 events: { onclick: () => switchLanguage() },
               })
             ),
-          ]),
-        ])
+            m(
+              'div.mobile-menu',
+              {
+                onclick: () => {
+                  this._mobileMenuShowing = !this._mobileMenuShowing;
+                },
+              },
+              m('img', { src: MobileMenuButton, alt: i18n('Menu') })
+            ),
+          ]
+        )
       ),
       m('section.grey'),
     ]);
+  }
+
+  static get _mainMenu() {
+    return m(
+      'ul.mainmenu',
+      mainNavigation.map((item, index) =>
+        m(
+          'li',
+          {
+            class: mainNavigation.selectedIndex === index ? 'active' : '',
+          },
+          [
+            m(
+              'a',
+              {
+                href: item.getLink(),
+                onupdate: item.onupdate,
+              },
+              i18n(item.label)
+            ),
+            item.submenu
+              ? m('ul.submenu', [
+                  item.submenu.map((subitem, subindex) =>
+                    m(
+                      'li',
+                      { class: item.submenu.selectedIndex === subindex ? 'active' : '' },
+                      m(
+                        'a',
+                        {
+                          href: subitem.getLink(),
+                          onupdate: subitem.onupdate,
+                        },
+                        i18n(subitem.label)
+                      )
+                    )
+                  ),
+                ])
+              : m(''),
+          ]
+        )
+      )
+    );
+  }
+
+  static get _profileMenu() {
+    return m(
+      'ul.profile',
+      isLoggedIn()
+        ? [
+            m(
+              'li',
+              {
+                class: m.route.get().includes(`/profile`) ? 'active' : '',
+              },
+              m(
+                'a',
+                { href: `/${currentLanguage()}/profile`, onupdate: m.route.link },
+                i18n('Profile')
+              )
+            ),
+            m(
+              'li',
+              m(
+                'a',
+                { href: `/${currentLanguage()}/logout`, onupdate: m.route.link },
+                i18n('Logout')
+              )
+            ),
+          ]
+        : [
+            m(
+              'li',
+              m(
+                'a',
+                { href: `/${currentLanguage()}/profile`, onupdate: m.route.link },
+                i18n('Login')
+              )
+            ),
+          ]
+    );
   }
 }
