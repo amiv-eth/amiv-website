b6d0fcb23db9be3b06543b9863f6fec1b49a33ce
Ilya <shatuncheg@gmail.com>
2018-10-13 13:40:55 +0200

    Style the job offer details section.
    1. Modify getDescription function to force h3 headers and capitalize them.
    2. Add styles for headers and lists.
    3. Remove company logo from job offer details section.

--- src/models/joboffers/Joboffer.js
+++ src/models/joboffers/Joboffer.js
@@ -29,7 +29,11 @@ export default class Joboffer {
 
   getDescription() {
     const otherLanguage = currentLanguage() === 'en' ? 'de' : 'en';
-    return this[`description_${currentLanguage()}`] || this[`description_${otherLanguage}`];
+    const description =
+      this[`description_${currentLanguage()}`] || this[`description_${otherLanguage}`];
+    // Use regex to substitute headers in description with h3 headers
+    const regex = /(#+\s+)(\w+)/g;
+    return description.replace(regex, (match, p1, p2) => `### ${p2.toLowerCase()}`);
   }
 
   getDate() {
