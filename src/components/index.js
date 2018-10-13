dfe3b5530bb11b0fbd86c85dff91e873fd0b2217
Sean Goff <sean.goff@energy.ch>
2018-10-13 14:18:42 +0200

    put RadioGroup into ui-component repo

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
-export { default as RadioButton } from './RadioButton';
 export { default as Tabs } from './Tabs';
 export { default as Dropdown } from './Dropdown';
 export { default as TextField } from './TextField';
