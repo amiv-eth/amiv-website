547dad31eac913ace4bf6b3226e13fe34cdf0378
Cliff Li <cliff@lin.li>
2018-10-13 10:23:06 +0200

    Improve look of default cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import { apiUrl } from 'config';
 import './Card.less';
+import AmivLogo from '../images/logoNoText.svg';
 
 export default class CheckboxComponent {
   constructor(vnode) {
@@ -11,7 +12,9 @@ export default class CheckboxComponent {
     const { title, href, img_poster } = this.item;
     let { imageurl } = this.item;
     if (img_poster) imageurl = `${apiUrl}${img_poster.file}`;
-    const style = imageurl ? `background-image: url(${imageurl})` : '';
+    const style = imageurl
+      ? `background-image: url(${imageurl})`
+      : `background-image: url(${AmivLogo})`;
 
     return m('a', { href }, m('div.card', { style }, m('div.card-title', title)));
   }
