e0006e11ad67cd58571d83a8783e3feb20076b5d
mlorin <mlorin@ethz.ch>
2018-10-14 15:12:16 +0200

    added styling to the facebook and twitter module

--- src/components/SocialMediaPlugin.js
+++ src/components/SocialMediaPlugin.js
@@ -1,8 +1,10 @@
 import m from 'mithril';
-import { apiUrl } from 'config';
+import moment from 'moment';
+import 'moment/locale/de-ch';
+
 import './SocialMediaPlugin.less';
 
-const request_url = "http://127.0.0.1:5000/socialApi/facebook?fields=posts{created_time%2Cmessage%2Cfull_picture}";
+const request_url = "http://127.0.0.1:5000/socialApi/facebook?fields=posts.limit(1){created_time,message,full_picture}";
 
 
 export default class SocialMediaComponent {
@@ -17,33 +19,35 @@ export default class SocialMediaComponent {
     if(title == "Twitter"){
      m.request({
          method: "GET",
-         url: "http://127.0.0.1:5000/socialApi/twitter",
+         url: "http://127.0.0.1:5000/socialApi/twitter?limit=1",
          extract: function(xhr) {return {status: xhr.status, body: xhr.responseText}}
      }).then(response => {
       this.content = m.trust(response.body);
-      loadScript("https://platform.twitter.com/widgets.js", function(){
-      });
+      loadScript("https://platform.twitter.com/widgets.js", function(){});
     });
    }else if(title == "Facebook"){
-      var result;
-
       m.request({
         method: "GET",
         url: request_url,
       }).then(response => {
-        console.log(response.posts.data)
-        result = m("div.facebook", [
-          m("a",{href},"Facebook"),
+        //console.log(response.posts.data)
+        
+        this.content = m("div.facebook", [
+          m("div.header",m("a",{href},"Facebook Feed")),
           response.posts.data.map(function(post) {
-              return m("li",[ 
-                  m("div", post.created_time),
-                  m("p",post.message),
-                  m("img",{src:post.full_picture},'')
+              var img_class = "";
+              if(post.full_picture){
+                img_class = ".picture";
+              }
+              
+              return m("div.element",[ 
+                  m("div.time", get_date(post.created_time)),
+                  m("img",{src:post.full_picture},''),
+                  m("p"+img_class,post.message),
                 ]
               );
-          })
+          }),
         ])
-        this.content = result;
       });
     }
   }
@@ -54,12 +58,14 @@ export default class SocialMediaComponent {
     if (title == "Twitter"){
       return m('div.socialMedia.twitter', this.content);
     }else if(title == "Facebook"){
-      console.log(this.content);
       return m('div.socialMedia', this.content);
     }
   }
 }
 
+function get_date(data){
+  return moment(data).fromNow();
+}
 
 //loading external script
 function loadScript(url, callback){
