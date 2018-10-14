eb37c7be806b4ec917a490c5b2a1c8ea7d15c36a
parethe <parethe@student.ethz.ch>
2018-10-14 16:22:51 +0200

    Studydocs style

--- src/views/studydocs/studydocDetails.js
+++ src/views/studydocs/studydocDetails.js
@@ -15,20 +15,60 @@ export default class StudydocDetails {
     }
 
     return [
-      m('table', [
-        m('tr', [m('td', m('b', i18n('studydocs.title'))), m('td', document.title)]),
-        m('tr', [m('td', m('b', i18n('studydocs.lecture'))), m('td', document.lecture)]),
-        m('tr', [m('td', m('b', i18n('studydocs.professor'))), m('td', document.professor)]),
-        m('tr', [m('td', m('b', i18n('studydocs.semester'))), m('td', document.semester)]),
-        m('tr', [m('td', m('b', i18n('studydocs.author'))), m('td', document.author)]),
-        m('tr', [m('td', m('b', i18n('studydocs.department'))), m('td', document.department)]),
-        m(Button, {
-          label: 'Download',
-          events: {
-            onclick: () => window.open(`${apiUrl}${document.files[0].file}`, '_blank'),
-          },
-        }),
+      m('div.studydoc-details-table', [
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.title'))),
+          m('div', document.title),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.type'))),
+          m('div', document.type),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.lecture'))),
+          m('div', document.lecture),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.professor'))),
+          m('div', document.professor),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.semester'))),
+          m('div', document.semester),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.author'))),
+          m('div', document.author),
+        ]),
+
+        m('div.flex-container', [
+          m('div', m('b', i18n('studydocs.department'))),
+          m('div', document.department),
+        ]),
       ]),
+
+      m(
+        'div.studydoc-details-table',
+        document.files.map(item =>
+          m('div', [
+            m(
+              'span.button-details-style',
+              m(Button, {
+                label: `Download ${item.name.split('.').pop()}`,
+                events: {
+                  onclick: () => window.open(`${apiUrl}${item.file}`, '_blank'),
+                },
+              })
+            ),
+            m('span#title-wrap-style', item.name),
+          ])
+        )
+      ),
     ];
   }
 }
