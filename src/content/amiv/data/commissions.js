0043758cc22b1ff6aa28b2bf9698fbf467a9af4a
Sandro Lutz <code@temparus.ch>
2018-11-07 19:34:03 +0100

    Update Team page content

--- src/content/amiv/data/commissions.js
+++ src/content/amiv/data/commissions.js
@@ -1,6 +1,10 @@
 // Contains static information about our commissions.
 // Markdown can be used to style the text.
 
+import bastliImage from '../images/commissions/bastli.png';
+import blitzImage from '../images/commissions/blitz.jpg';
+import emc2Image from '../images/commissions/emc2.jpg';
+
 const data = [
   {
     name: 'Bastli',
@@ -9,12 +13,14 @@ const data = [
 
           Wir sind für alle da und helfen euch gerne. Vorkentnisse werden keine benötigt, Hauptsache ihr habt Spass daran Dinge zu bauen ;)`,
     },
+    image: bastliImage,
     website: 'https://bastli.ethz.ch',
     email: 'info@bastli.ethz.ch',
     phone: '+41 44 632 49 41',
   },
   {
     name: 'Blitz',
+    image: blitzImage,
     website: 'https://blitz.ethz.ch',
     email: 'info@blitz.ethz.ch',
   },
@@ -52,7 +58,7 @@ const data = [
       en: 'Not a real description.',
       de: 'Keine echte Beschreibung',
     },
-    website: 'https://limes.amiv.ethz.ch',
+    website: 'https://limes.ethz.ch',
     email: 'limes@amiv.ethz.ch',
   },
   {
@@ -71,9 +77,16 @@ const data = [
   },
   {
     name: 'E=MC^2',
+    image: emc2Image,
+    email: 'emc2@amiv.ethz.ch',
+    website: 'https://fb.me/emc2eth',
     description: {
-      en: 'Not a real description.',
-      de: 'Keine echte Beschreibung',
+      en: `The Energy Master Chill Club is a student-run AMIV commission bringing together energy students across different years through all types of events (Fondue Night, BBQ, Ski Weekend, etc.).
+
+        Our events are not just for MEST students, we encourage any and all students to join us!`,
+      de: `Der Energy Master Chill Club ist eine studentische AMIV-Kommission, welche MEST-Studenten von allen Jahrgängen bei verschiedenen Events zusammenbringt (Fondueabend, BBQ, Skiwochenende, etc.).
+
+        Unsere Event sind nicht exklusiv für MEST-Studenten, wir freuen uns über alle TeilnehmerInnen!`,
     },
   },
   {
--- src/content/amiv/data/ressorts.js
+++ src/content/amiv/data/ressorts.js
@@ -1,9 +1,13 @@
 // Contains static information about our ressorts.
 // Markdown can be used to style the text.
 
+import kulturImage from '../images/ressorts/kultur.png';
+
 const data = [
   {
     name: 'Designteam',
+    email: 'design@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
     description: {
       en: 'Not a real description.',
       de: 'Keine echte Beschreibung',
@@ -11,6 +15,8 @@ const data = [
   },
   {
     name: 'ER Team',
+    email: 'er@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
     description: {
       en: 'Not a real description.',
       de: 'Keine echte Beschreibung',
@@ -18,6 +24,8 @@ const data = [
   },
   {
     name: 'IT Team',
+    email: 'it@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
     description: {
       en: 'Not a real description.',
       de: 'Keine echte Beschreibung',
@@ -25,6 +33,34 @@ const data = [
   },
   {
     name: 'Kulturteam',
+    image: kulturImage,
+    email: 'kultur@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
+    description: {
+      en: `The AMIV culture branch brings you more than 30 events every semester:
+        from legendary happenings such as our Sushi-Night, AMIVondue to Lasertag, theatre visits and speed dating, there is something for everyone!
+
+        Do you have a brilliant idea for a new event that we missed and should be holding? Or would you like to help us organise an event?
+        Send us an email or drop by the AMIV offices and join the AMIV culture branch!`,
+      de: `Wir vom Kulturteam organisieren für euch über 30 Events pro Semester.
+        Von Sushi-Night und AMIVondue über Lasertag bis zu Theater und Speeddating hat es bestimmt für jeden etwas dabei!
+      
+        Hast du eine geniale Idee für ein Event, das dir noch fehlt? Oder möchtest du gerne selbst einmal Event-Luft schnuppern und mithelfen ein Event zu planen? Dann melde dich doch per Mail bei uns oder schau einfach mal im AMIV-Büro vorbei!`,
+    },
+  },
+  {
+    name: 'HoPo MAVT',
+    email: 'hopo-mavt@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
+    description: {
+      en: 'Not a real description.',
+      de: 'Keine echte Beschreibung',
+    },
+  },
+  {
+    name: 'HoPo ITET',
+    email: 'hopo-itet@amiv.ethz.ch',
+    phone: '+41 44 632 42 45',
     description: {
       en: 'Not a real description.',
       de: 'Keine echte Beschreibung',
