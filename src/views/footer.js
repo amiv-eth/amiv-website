806e90514e4375014fa8d4c38ea3d7452acd92e1
Cliff Li <lic@student.ethz.ch>
2018-11-10 12:20:07 +0100

    Add logos to footer

--- src/views/footer.js
+++ src/views/footer.js
@@ -1,12 +1,13 @@
 import m from 'mithril';
 import { currentLanguage, i18n } from '../models/language';
+import EthLogo from '../images/eth.svg';
+import VsethLogo from '../images/vseth.svg';
 
 export default class Footer {
   static view() {
-    return m(
-      'footer',
-      m('div', [
-        m('span.copyright', `© 1893 - ${new Date().getFullYear()} AMIV an der ETH`),
+    return m('footer', [
+      m('div.copyright', [
+        m('span', `© 1893 - ${new Date().getFullYear()} AMIV an der ETH`),
         m(
           'a',
           {
@@ -15,7 +16,12 @@ export default class Footer {
           },
           i18n('legal-notice')
         ),
-      ])
-    );
+      ]),
+      m('div.footer-logo', m('a', { href: `https://www.ethz.ch/` }, m('img', { src: EthLogo }))),
+      m(
+        'div.footer-logo',
+        m('a', { href: `https://vseth.ethz.ch/` }, m('img', { src: VsethLogo }))
+      ),
+    ]);
   }
 }
